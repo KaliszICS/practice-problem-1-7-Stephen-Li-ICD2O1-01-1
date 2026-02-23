00515636b313728c8a89dcf105806c4bce298c1b
Stephen-Li-ICD2O1-01 <stephen.li@student.tdsb.on.ca>
2026-02-23 16:39:12 +0000

    save

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,3 +1,12 @@
+/**
+File: Lesson 1.7 - Booleans (and, or, not)
+Author: Stephen li
+Date Created: Feb 23, 2026
+Date Last Modified: Feb 23, 2026
+*/
+
+import java.util.Scanner;
+
 public class PracticeProblem {
 
 	public static void main(String args[]) {
@@ -10,27 +19,49 @@ public class PracticeProblem {
 	}
 
 	public static void q1() {
-		//Write question 1 code here
+		boolean bool1 = true;
+		boolean bool2 = false;
+		System.out.println(bool1 && bool2);
+		System.out.println(bool1 || bool2);
 	}
 
 	public static void q2() {
-		//Write question 2 code here
+		Scanner input = new Scanner(System.in);
+		System.out.print("Input a lowercase letter: ");
+		String word1 = input.nextLine();
+		char char1 = word1.charAt(0);
+		System.out.print("Input another lowercase letter: ");
+		String word2 = input.nextLine();
+		char char2 = word2.charAt(0);
+		System.out.println(char1 < char2);
 	}
 
 	public static void q3() {
-		//Write question 3 code here
+		Scanner input = new Scanner(System.in);
+		System.out.print("Input a number: ");
+		double num1 = input.nextDouble();
+		System.out.println((num1 >= 0) && (num1 <= 10));
 	}
 
 	public static void q4() {
-		//Write question 4 code here
+		Scanner input = new Scanner(System.in);
+		System.out.print("Input a number: ");
+		double num1 = input.nextDouble();
+		System.out.println((num1 > 0) && (num1 != 5));
 	}
 
 	public static void q5() {
-		//Write question 5 code here
+		Scanner input = new Scanner(System.in);
+		System.out.print("Input a word: ");
+		String word1 = input.nextLine();
+		System.out.println(word1.compareTo("banana") != 0);
 	}
 
 	public static void q6() {
-		//Write question 5 code here
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter an integer: ");
+		int num1 = input.nextInt();
+		System.out.println("The integer " + num1 + " is " + (num1 % 2 == 0));
 	}
 
 }
